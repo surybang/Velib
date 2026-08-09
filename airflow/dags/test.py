a8f4e3dac2afcd6b4cd9602690235f4214ed1bb6
surybang <hos.fabien@outlook.com>
2026-08-09 22:11:09 +0000

    test: airflow dag

---
+++ airflow/dags/test.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+import pendulum
+
+with DAG(
+    dag_id="test_pod_permissions",
+    schedule=None,          # déclenché à la main
+    start_date=pendulum.datetime(2026, 8, 1, tz="Europe/Paris"),
+    catchup=False,
+) as dag:
+    KubernetesPodOperator(
+        task_id="hello",
+        name="hello-pod",
+        image="busybox",
+        cmds=["sh", "-c", "echo ok"],
+        in_cluster=True,
+        get_logs=True,
+    )
